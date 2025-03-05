526919d43da32ff2c85d4c1570d1da7b8c2520f8
Philipp Wolfger <philipp.wolfger@alturos.com>
2025-03-05 16:51:58 +0100

    - Fixed leaderboard unit tests - Added leaderboard integration tests

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerIntegrationTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerIntegrationTests.kt
@@ -3,21 +3,19 @@ package at.aau.serg.controllers
 import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.junit.jupiter.api.BeforeEach
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.spy
 import org.mockito.Mockito.verify
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import org.mockito.Mockito.`when` as whenever // when is a reserved keyword in Kotlin
 
-class LeaderboardControllerTests {
-
-    private lateinit var mockedService: GameResultService
+class LeaderboardControllerIntegrationTests {
+    private lateinit var gameResultService: GameResultService
     private lateinit var controller: LeaderboardController
 
     @BeforeEach
     fun setup() {
-        mockedService = mock<GameResultService>()
-        controller = LeaderboardController(mockedService)
+        gameResultService = spy(GameResultService())
+        controller = LeaderboardController(gameResultService)
     }
 
     @Test
@@ -26,11 +24,13 @@ class LeaderboardControllerTests {
         val second = GameResult(2, "second", 15, 10.0)
         val third = GameResult(3, "third", 10, 15.0)
 
-        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+        gameResultService.addGameResult(first)
+        gameResultService.addGameResult(second)
+        gameResultService.addGameResult(third)
 
         val res: List<GameResult> = controller.getLeaderboard()
 
-        verify(mockedService).getGameResults()
+        verify(gameResultService).getGameResults()
         assertEquals(3, res.size)
         assertEquals(first, res[0])
         assertEquals(second, res[1])
@@ -43,15 +43,16 @@ class LeaderboardControllerTests {
         val second = GameResult(2, "second", 20, 10.0)
         val third = GameResult(3, "third", 20, 15.0)
 
-        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+        gameResultService.addGameResult(first)
+        gameResultService.addGameResult(second)
+        gameResultService.addGameResult(third)
 
         val res: List<GameResult> = controller.getLeaderboard()
 
-        verify(mockedService).getGameResults()
+        verify(gameResultService).getGameResults()
         assertEquals(3, res.size)
         assertEquals(second, res[0])
         assertEquals(third, res[1])
         assertEquals(first, res[2])
     }
-
 }
